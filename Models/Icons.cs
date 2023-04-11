aed88006320d75778ccd83eaa1f2b3caaa2ea39f
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-04-11 08:57:12 +0200

    pushing...

---
+++ Models/Icons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Models
+{
+    public static class Icons
+    {
+        public const string Dashboard = "dashboard.svg";
+        public const string Home = "home.svg";
+        public const string AboutUs = "about_us.svg";
+        public const string Settings = "settings.svg";
+    }
+}
